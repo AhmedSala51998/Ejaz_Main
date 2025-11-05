ef881df775515a937fb88a6f2420310e21e29363
git <ahmedsalah511998@gmail.com>
2025-11-05 18:25:13 +0300

    save_changes_colors_dashboard

--- resources/views/admin/crud/biographies/index.blade.php
+++ resources/views/admin/crud/biographies/index.blade.php
@@ -329,7 +329,7 @@
                 <select class="form-select select2" id="marketer" name="marketer" style="width:100%;"></select>
             </div>
 
-            <button type="submit" class="btn w-100" style="background-color: #ff6600; color: #fff; border: none;">
+            <button type="submit" class="btn w-100" style="background-color: #f4a835; color: #fff; border: none;">
             تأكيد الحجز
             </button>
             </form>
