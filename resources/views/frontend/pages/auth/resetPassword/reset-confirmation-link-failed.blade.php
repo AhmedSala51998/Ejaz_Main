d6ef990c75dfa79e6718c4162d2edb0cd00bbb9f
git <ahmedsalah511998@gmail.com>
2025-12-06 18:58:50 +0300

    save_expired_page_changes

--- resources/views/frontend/pages/auth/resetPassword/reset-confirmation-link-failed.blade.php
+++ resources/views/frontend/pages/auth/resetPassword/reset-confirmation-link-failed.blade.php
@@ -10,19 +10,18 @@
 
 
 @section('content')
-    <section class="account">
-        <div class="container">
-            <div class="formCard row">
-                <div class="circleBlur"></div>
-                <div class="circleBlur2"></div>
-                <div class="col-md-12 p-2 text-center">
-                    <img class="loginImg" style="height: 120px!important;width: 120px!important;" src="{{asset('frontend/img/expired.png')}}" alt="">
-                    <h6 class="text-center mt-4 mb-4">{{__('frontend.The page has expired')}}</h6>
-                </div>
+<section class="account" style="min-height: 70vh; display: flex; align-items: center; justify-content: center;">
+    <div class="container">
+        <div class="formCard row" style="position: relative;">
+            <div class="circleBlur"></div>
+            <div class="circleBlur2"></div>
+            <div class="col-md-12 p-2 text-center">
+                <img class="loginImg" style="height: 120px!important;width: 120px!important;" src="{{asset('frontend/img/expired.png')}}" alt="">
+                <h6 class="text-center mt-4 mb-4">{{__('frontend.The page has expired')}}</h6>
             </div>
         </div>
-
-    </section>
+    </div>
+</section>
 @endsection
 
 @section('js')
