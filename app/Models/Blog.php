d7f06690165fd4f4d0a6f9f41f5d8ece3674512e
git <ahmedsalah511998@gmail.com>
2025-12-15 18:24:08 +0300

    hgjhgjghj

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Translatable\HasTranslations;
 
 class Blog extends Model
 {
     use HasFactory;
-    use HasTranslations;
 
     protected $table = 'blogs';
 
