a3fcd4c83ba8a15ab495c332309612d784c0542e
git <ahmedsalah511998@gmail.com>
2025-12-29 12:25:09 +0300

    jhgjghjghjg

--- resources/views/frontend/pages/all-workers/all-workers.blade.php
+++ resources/views/frontend/pages/all-workers/all-workers.blade.php
@@ -540,7 +540,7 @@
 
         /*New Filter Design */
         .filter-wrapper {
-            max-width: 1140px;
+            max-width: 1400px;
             margin: 0 auto;
         }
         .horizontal-filter {
