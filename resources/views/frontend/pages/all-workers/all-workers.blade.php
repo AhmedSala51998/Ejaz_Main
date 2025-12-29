63ad663b9a1bb56a1cc245a9e630839eb15703cc
git <ahmedsalah511998@gmail.com>
2025-12-29 12:26:51 +0300

    hgjghjghjhg

--- resources/views/frontend/pages/all-workers/all-workers.blade.php
+++ resources/views/frontend/pages/all-workers/all-workers.blade.php
@@ -540,7 +540,7 @@
 
         /*New Filter Design */
         .filter-wrapper {
-            max-width: 1400px;
+            max-width: 1500px;
             margin: 0 auto;
         }
         .horizontal-filter {
