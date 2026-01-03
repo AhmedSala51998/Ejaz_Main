33384199544616198ebeece1b79231a7b3a3dd2c
git <ahmedsalah511998@gmail.com>
2026-01-03 21:08:20 +0300

    hjgjghjghjgh

--- resources/views/frontend/layouts/layout.blade.php
+++ resources/views/frontend/layouts/layout.blade.php
@@ -551,7 +551,7 @@
         /* ===== RAMADAN SVG DECOR ===== */
         .ramadan-svg-decor{
         position: fixed;
-        top: 80px;
+        top: 90px;
         left: 0;
         width: 100%;
         height: 180px;
