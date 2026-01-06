dc2849f91b155f033e595ecedea98ce0317cfd3e
git <ahmedsalah511998@gmail.com>
2026-01-06 14:33:02 +0300

    save_updating_cv_image_box

--- resources/views/frontend/pages/all-workers/worker/worker_component.blade.php
+++ resources/views/frontend/pages/all-workers/worker/worker_component.blade.php
@@ -459,19 +459,16 @@
 
 .cv-image-wrapper {
     width: 100%;
-    height: 620px;
     display: flex;
     align-items: center;
     justify-content: center;
     background: #fff;
-    overflow: hidden;
 }
+
 .cv-image-wrapper img {
-    max-width: 100%;
-    max-height: 100%;
-    width: auto;
+    width: 100%;
     height: auto;
-    object-fit: contain;
+    display: block;
 }
 </style>
 
