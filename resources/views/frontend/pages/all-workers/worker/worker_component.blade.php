a602f66ef0082a05ffff616d484449327c615dec
git <ahmedsalah511998@gmail.com>
2026-01-15 12:46:26 +0300

    save_changes_performance_seo_v4

--- resources/views/frontend/pages/all-workers/worker/worker_component.blade.php
+++ resources/views/frontend/pages/all-workers/worker/worker_component.blade.php
@@ -247,7 +247,7 @@ img {
                     <a data-fancybox="users{{$cv->id}}-CV" href="{{ get_file($cv->cv_file) }}">
                         <div class="cv-image-wrapper">
 
-                                <img src="{{ get_file($cv->cv_file) }}" width="1200" height="1300" loading="lazy" decoding="async" alt="CV Image">
+                                <img src="{{ get_file($cv->cv_file) }}" width="1200" height="800" loading="lazy" decoding="async" alt="CV Image">
 
                         </div>
                     </a>
