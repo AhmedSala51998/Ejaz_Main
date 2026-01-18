1b71f3b41b6a10df7b96ba0729c7096d102f92f6
git <ahmedsalah511998@gmail.com>
2026-01-18 14:59:55 +0300

    save_changes_performance_seo_v60

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -1,14 +1,18 @@
-<link rel="icon" type="image/x-icon" href="{{$settings->tap_logo?get_file($settings->tap_logo):asset('frontend/img/fav.svg')}}">
+<link rel="icon" type="image/x-icon" href="{{$settings->tap_logo ? get_file($settings->tap_logo) : asset('frontend/img/fav.svg')}}">
 
-
-
-<!-- dropify -->
-<link rel="stylesheet" href="{{asset('frontend')}}/css/dropify.min.css">
-
+<style>
+@font-face {
+    font-family: 'Cairo';
+    src: url('{{asset("frontend/fonts/Cairo.woff2")}}') format('woff2'),
+         url('{{asset("frontend/fonts/Cairo.woff")}}') format('woff');
+    font-display: swap;
+}
+</style>
 
 <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/css/fontawesome.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
+
 <link rel="preload" href="{{asset('frontend/css/swiper-bundle.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/animate.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/aos.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
@@ -35,81 +39,32 @@
   <link rel="stylesheet" href="{{asset('frontend/css/introjs-rtl.css')}}">
 </noscript>
 
-
 <style>
-    @font-face {
-        font-family: 'Cairo';
-        src: url('{{asset("frontend/fonts/Cairo.woff2")}}') format('woff2'),
-            url('{{asset("frontend/fonts/Cairo.woff")}}') format('woff');
-        font-display: swap;
-    }
+.form-error { color: red; font-weight: normal; font-size: smaller; }
+input.valid, textarea.valid { border: 1px solid green; }
 
-    .form-error{
-        color: red;
-        font-weight: normal;
-        font-size: smaller;
-    }
+/* Floating buttons */
+.container-wa .floating-button {
+    position: fixed; bottom: 40px; right: 40px;
+    width: 60px; height: 60px;
+    background-color: #25d366; color: white;
+    border-radius: 50px; text-align: center;
+    font-size: 35px; cursor: pointer;
+    box-shadow: 0px 2px 5px #666;
+}
+.container-wa .floating-button:hover { background-color: #1fad53; }
+.container-wa .floating-button .icon { position: absolute; top:0; left:0; right:0; line-height:60px; text-align:center; transition: all 0.3s; }
+.container-wa .floating-button .icon.wa { animation: wa-out 0.3s; }
 
-    input.valid, textarea.valid{
-        border: 1px solid green;
-    }
-    .container-wa .floating-button {
-        position: fixed;
-        bottom: 40px;
-        right: 40px;
-        width: 60px;
-        height: 60px;
-        background-color: #25d366;
-        color: white;
-        border-radius: 50px;
-        text-align: center;
-        font-size: 35px;
-        cursor: pointer;
-        box-shadow: 0px 2px 5px #666;
-    }
-    .container-wa .floating-button:hover {
-        background-color: #1fad53;
-    }
-    .container-wa .floating-button .icon {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        line-height: 60px;
-        text-align: center;
-        transition: all 0.3s;
-    }
-    .container-wa .floating-button .icon.wa {
-        animation: wa-out 0.3s;
-    }
-    .container-call .floating-button {
-        position: fixed;
-        bottom: 40px;
-        left: 40px;
-        width: 60px;
-        height: 60px;
-        background-color: #1a3354;
-        color: white;
-        border-radius: 50px;
-        text-align: center;
-        font-size: 30px;
-        cursor: pointer;
-        box-shadow: 0px 2px 5px #666;
-    }
-    .container-call .floating-button:hover {
-        background-color: #166fbf;
-    }
-    .container-call .floating-button i{
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        line-height: 60px;
-        text-align: center;
-        transition: all 0.3s;
-    }
-    .container-call .floating-button .icon.call {
-        animation: wa-out 0.3s;
-    }
-
+.container-call .floating-button {
+    position: fixed; bottom:40px; left:40px;
+    width:60px; height:60px;
+    background-color:#1a3354; color:white;
+    border-radius:50px; text-align:center;
+    font-size:30px; cursor:pointer;
+    box-shadow:0px 2px 5px #666;
+}
+.container-call .floating-button:hover { background-color:#166fbf; }
+.container-call .floating-button i { position:absolute; top:0; left:0; right:0; line-height:60px; text-align:center; transition:all 0.3s; }
+.container-call .floating-button .icon.call { animation: wa-out 0.3s; }
 </style>
