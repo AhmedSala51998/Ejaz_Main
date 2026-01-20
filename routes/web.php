bdc6a9a0099a1e9fa681cb23cd1da00b59a22a84
git <ahmedsalah511998@gmail.com>
2026-01-20 12:03:00 +0300

    save_changes_performance_seo_v121

--- routes/web.php
+++ routes/web.php
@@ -264,7 +264,7 @@ Route::group(
 
     ## support
 
-    //Route::get('supports',[\App\Http\Controllers\Frontend\Support\SupportFrontController::class,'supports'])->name('frontend.supports');
+    Route::get('supports',[\App\Http\Controllers\Frontend\Support\SupportFrontController::class,'supports'])->name('frontend.supports');
 
 
     ## contactUs Support
