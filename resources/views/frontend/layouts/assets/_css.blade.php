ccbdf36d4b6c6a5527890cdbf11fa2a91796f4f2
git <ahmedsalah511998@gmail.com>
2026-01-25 18:08:59 +0300

    save_changes_performmance_seo_v246

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -1,7 +1,6 @@
 <link rel="icon" type="image/x-icon" href="{{$settings->tap_logo ? get_file($settings->tap_logo) : asset('frontend/img/fav.svg')}}">
 
 <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
-<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
 
 <link rel="preload" href="{{asset('frontend/css/fontawesome.min.css')}}" as="style" onload="this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/swiper-bundle.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
