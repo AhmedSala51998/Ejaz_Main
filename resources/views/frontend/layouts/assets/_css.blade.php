4274f9714c8758a8b646a23eab498f161029f268
git <ahmedsalah511998@gmail.com>
2026-01-25 18:48:00 +0300

    save_changes_performance_seo_v253

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -1,22 +1,35 @@
 <link rel="icon" type="image/x-icon" href="{{$settings->tap_logo ? get_file($settings->tap_logo) : asset('frontend/img/fav.svg')}}">
 
+
 <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
 
 <link rel="preload" href="{{asset('frontend/css/fontawesome.min.css')}}" as="style" onload="this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/swiper-bundle.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{asset('frontend/css/animate.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{asset('frontend/css/aos.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{asset('frontend/css/dropify.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{asset('frontend/css/jquery.fancybox.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/odometer.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/select2.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/backEndFiles/validation/toastr.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{asset('frontend/css/introjs.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="{{asset('frontend/css/introjs-rtl.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 
 
 <noscript>
   <link rel="stylesheet" href="{{asset('frontend/css/swiper-bundle.min.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/animate.min.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/aos.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/dropify.min.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/jquery.fancybox.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/odometer.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/select2.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/backEndFiles/validation/toastr.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/introjs.css')}}">
+  <link rel="stylesheet" href="{{asset('frontend/css/introjs-rtl.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/fontawesome.min.css')}}">
 </noscript>
 
