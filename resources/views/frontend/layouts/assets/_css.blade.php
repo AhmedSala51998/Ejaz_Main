ca54746126cc1430412f1363f044e3d304eab879
git <ahmedsalah511998@gmail.com>
2026-01-25 18:41:08 +0300

    save_changes_performance_seo_v251

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -1,6 +1,5 @@
 <link rel="icon" type="image/x-icon" href="{{$settings->tap_logo ? get_file($settings->tap_logo) : asset('frontend/img/fav.svg')}}">
 
-<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
 
 <link rel="preload" href="{{asset('frontend/css/fontawesome.min.css')}}" as="style" onload="this.rel='stylesheet'">
