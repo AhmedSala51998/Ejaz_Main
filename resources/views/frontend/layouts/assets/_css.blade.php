9ae44352a7f75f814b03f96cb307fb5e38b2e160
git <ahmedsalah511998@gmail.com>
2026-01-26 11:23:58 +0300

    save_changes_performance_seo_v257

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -4,44 +4,37 @@
 <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
 
-<link rel="preload" href="{{asset('frontend/css/fontawesome.min.css')}}" as="style" onload="this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/swiper-bundle.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/animate.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/aos.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/dropify.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/jquery.fancybox.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/odometer.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/select2.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/backEndFiles/validation/toastr.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/introjs.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/introjs-rtl.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<script>
+(function () {
+  const styles = [
+    "{{asset('frontend/css/fontawesome.min.css')}}",
+    "{{asset('frontend/css/swiper-bundle.min.css')}}",
+    "{{asset('frontend/css/animate.min.css')}}",
+    "{{asset('frontend/css/aos.css')}}",
+    "{{asset('frontend/css/dropify.min.css')}}",
+    "{{asset('frontend/css/jquery.fancybox.min.css')}}",
+    "{{asset('frontend/css/odometer.min.css')}}",
+    "{{asset('frontend/css/select2.min.css')}}",
+    "{{asset('frontend/backEndFiles/validation/toastr.min.css')}}",
+    "{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}",
+    "{{asset('frontend/css/introjs.css')}}",
+    "{{asset('frontend/css/introjs-rtl.css')}}"
+  ];
+
+  styles.forEach(href => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    link.media = 'print';
+    link.onload = () => link.media = 'all';
+    document.head.appendChild(link);
+  });
+})();
+</script>
 
 
-<noscript>
-  <link rel="stylesheet" href="{{asset('frontend/css/swiper-bundle.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/animate.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/aos.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/dropify.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/jquery.fancybox.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/odometer.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/select2.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/backEndFiles/validation/toastr.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/introjs.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/introjs-rtl.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/fontawesome.min.css')}}">
-</noscript>
-
-<link rel="preload"
-      as="style"
-      href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;700;800&family=Tajawal:wght@400;500;700&display=swap"
-      onload="this.onload=null;this.rel='stylesheet'">
-
-<noscript>
 <link rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;700;800&family=Tajawal:wght@400;500;700&display=swap">
-</noscript>
 
 
 <style>
