da47d6dd5bbf848f3e09eef68a75ac21e31404d3
git <ahmedsalah511998@gmail.com>
2026-01-28 13:34:49 +0300

    save_changes_performance_seo_v285

--- resources/views/frontend/pages/home/parts/ourStatistics.blade.php
+++ resources/views/frontend/pages/home/parts/ourStatistics.blade.php
@@ -149,14 +149,14 @@
                 </div>
                 <div class="col-6 col-md-3 p-2">
                     <div class="specifications wow fadeInUp">
-                        <i class="fab fa-buildings"></i>
+                        <i class="fa fa-buildings"></i>
                         <h1 class="odometer" data-count="812">00</h1>
                         <h6> زوارنا </h6>
                     </div>
                 </div>
                 <div class="col-6 col-md-3 p-2">
                     <div class="specifications wow fadeInUp">
-                        <i class="fab fa-user-headset"></i>
+                        <i class="fa fa-user-headset"></i>
                         <h1 class="odometer" data-count="793">00</h1>
                         <h6> خدمة العملاء </h6>
                     </div>
