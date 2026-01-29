b35f5b3c07c0968fa18f4c1ac9d28312d2206d9a
git <ahmedsalah511998@gmail.com>
2026-01-29 12:20:51 +0300

    save_changes_performance_seo_v300

--- resources/views/frontend/pages/home/parts/ourStatistics.blade.php
+++ resources/views/frontend/pages/home/parts/ourStatistics.blade.php
@@ -43,11 +43,10 @@
 }
 
 .statistics .specifications i {
-    font-size: 44px !important;
-    color: #ff9800 !important;
+    font-size: 44px;
+    color: #ff9800;
     margin-bottom: 20px;
     display: block;
-    margin-left:5px !important;
 }
 
 .statistics .specifications h1 {
@@ -93,13 +92,6 @@
     max-width: 100%;
     flex: 0 0 100%;
   }
-    .statistics .specifications i {
-        font-size: 44px !important;
-        color: #ff9800 !important;
-        margin-bottom: 20px;
-        display: block;
-        margin-left:0 !important;
-    }
 }
 @media (max-width: 767px) {
     .circleBlur,
@@ -129,7 +121,7 @@
                 @foreach($statistics as $statistic)
                 <div class="col-12 col-md-3 p-2">
                     <div class="specifications wow fadeInUp">
-                        <i class="fa {{$statistic->icon}}"></i>
+                        <i style="font-size: 44px !important;color:#d97706 !important;margin-left:5px !important" class="fa {{$statistic->icon}}"></i>
                         <h1 class="odometer" data-count="{{$statistic->number}}">00</h1>
                         <h6> {{$statistic->title}} </h6>
                     </div>
