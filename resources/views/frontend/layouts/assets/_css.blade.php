0331aa9ea912e7051d195fbb22f715eff57149b2
git <ahmedsalah511998@gmail.com>
2026-01-29 13:27:03 +0300

    save_changes_performance_seo_v305

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -1,19 +1,6 @@
 <link rel="icon" type="image/x-icon" href="{{$settings->tap_logo ? get_file($settings->tap_logo) : asset('frontend/img/fav.svg')}}">
 
 
-<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
-<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
-
-<link rel="preload"
-      href="{{asset('frontend/css/all.min.css')}}"
-      as="style"
-      onload="this.onload=null;this.rel='stylesheet'">
-
-<noscript>
-  <link rel="stylesheet" href="{{asset('frontend/css/all.min.css')}}">
-</noscript>
-
-<link rel="preload" href="{{asset('frontend/css/fontawesome.min.css')}}" as="style" onload="this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/swiper-bundle.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/animate.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/aos.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
@@ -39,7 +26,6 @@
   <link rel="stylesheet" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/introjs.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/introjs-rtl.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/fontawesome.min.css')}}">
 </noscript>
 
 <link rel="preload"
