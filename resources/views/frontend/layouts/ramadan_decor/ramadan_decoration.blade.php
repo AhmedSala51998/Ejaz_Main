05a4298946de192b2764a30cd56598734c94e3d0
git <ahmedsalah511998@gmail.com>
2026-02-08 15:28:45 +0300

    jghjhjkhjkhj

--- resources/views/frontend/layouts/ramadan_decor/ramadan_decoration.blade.php
+++ resources/views/frontend/layouts/ramadan_decor/ramadan_decoration.blade.php
@@ -1,35 +1,37 @@
 <!-- RAMADAN BUNTING DECOR -->
-<div class="ramadan-top-decor">
-  <svg viewBox="0 0 1920 180" preserveAspectRatio="none"
-       xmlns="http://www.w3.org/2000/svg">
+<div class="ramadan-bunting-wrapper" aria-hidden="true">
+    <div class="ramadan-top-decor">
+    <svg viewBox="0 0 1920 180" preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg">
 
-    <!-- Rope -->
-    <path id="rope" d="M0 80 Q960 150 1920 80"
-          stroke="#f4b400"
-          stroke-width="1"
-          fill="none"/>
+        <!-- Rope -->
+        <path id="rope" d="M0 80 Q960 150 1920 80"
+            stroke="#f4b400"
+            stroke-width="1"
+            fill="none"/>
 
-    <!-- Flags -->
-    <g id="flags">
-        <polygon fill="#f7c400"/>
-        <polygon fill="#4fc3f7"/>
-        <polygon fill="#7cb342"/>
-        <polygon fill="#f7c400"/>
-        <polygon fill="#4fc3f7"/>
-        <polygon fill="#7cb342"/>
-        <polygon fill="#f7c400"/>
-        <polygon fill="#4fc3f7"/>
-        <polygon fill="#7cb342"/>
-        <polygon fill="#f7c400"/>
-        <polygon fill="#4fc3f7"/>
-        <polygon fill="#7cb342"/>
-        <polygon fill="#f7c400"/>
-        <polygon fill="#4fc3f7"/>
-        <polygon fill="#7cb342"/>
-        <polygon fill="#f7c400"/>
-        <polygon fill="#4fc3f7"/>
-        <polygon fill="#7cb342"/>
-    </g>
+        <!-- Flags -->
+        <g id="flags">
+            <polygon fill="#f7c400"/>
+            <polygon fill="#4fc3f7"/>
+            <polygon fill="#7cb342"/>
+            <polygon fill="#f7c400"/>
+            <polygon fill="#4fc3f7"/>
+            <polygon fill="#7cb342"/>
+            <polygon fill="#f7c400"/>
+            <polygon fill="#4fc3f7"/>
+            <polygon fill="#7cb342"/>
+            <polygon fill="#f7c400"/>
+            <polygon fill="#4fc3f7"/>
+            <polygon fill="#7cb342"/>
+            <polygon fill="#f7c400"/>
+            <polygon fill="#4fc3f7"/>
+            <polygon fill="#7cb342"/>
+            <polygon fill="#f7c400"/>
+            <polygon fill="#4fc3f7"/>
+            <polygon fill="#7cb342"/>
+        </g>
 
-  </svg>
+    </svg>
+    </div>
 </div>
