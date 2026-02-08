9f96140359831e174029591eb69ad8b1e56f15ba
git <ahmedsalah511998@gmail.com>
2026-02-08 17:31:22 +0300

    mjkhjhjkhjkjh

--- resources/views/frontend/layouts/ramadan_decor/ramadan_decoration.blade.php
+++ resources/views/frontend/layouts/ramadan_decor/ramadan_decoration.blade.php
@@ -1,37 +1,103 @@
 <!-- RAMADAN BUNTING DECOR -->
-<div class="ramadan-bunting-wrapper" aria-hidden="true">
-    <div class="ramadan-top-decor">
-    <svg viewBox="0 0 1920 180" preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg">
+<div class="ramadan-top-decor">
+  <svg viewBox="0 0 1920 180" preserveAspectRatio="none"
+       xmlns="http://www.w3.org/2000/svg">
 
-        <!-- Rope -->
-        <path id="rope" d="M0 80 Q960 150 1920 80"
-            stroke="#f4b400"
-            stroke-width="1"
-            fill="none"/>
+    <!-- Rope -->
+    <path id="rope" d="M0 80 Q960 150 1920 80"
+          stroke="#f4b400"
+          stroke-width="1"
+          fill="none"/>
 
-        <!-- Flags -->
-        <g id="flags">
-            <polygon fill="#f7c400"/>
-            <polygon fill="#4fc3f7"/>
-            <polygon fill="#7cb342"/>
-            <polygon fill="#f7c400"/>
-            <polygon fill="#4fc3f7"/>
-            <polygon fill="#7cb342"/>
-            <polygon fill="#f7c400"/>
-            <polygon fill="#4fc3f7"/>
-            <polygon fill="#7cb342"/>
-            <polygon fill="#f7c400"/>
-            <polygon fill="#4fc3f7"/>
-            <polygon fill="#7cb342"/>
-            <polygon fill="#f7c400"/>
-            <polygon fill="#4fc3f7"/>
-            <polygon fill="#7cb342"/>
-            <polygon fill="#f7c400"/>
-            <polygon fill="#4fc3f7"/>
-            <polygon fill="#7cb342"/>
-        </g>
+    <!-- Flags -->
+    <g id="flags">
+        <polygon fill="#f7c400"/>
+        <polygon fill="#4fc3f7"/>
+        <polygon fill="#7cb342"/>
+        <polygon fill="#f7c400"/>
+        <polygon fill="#4fc3f7"/>
+        <polygon fill="#7cb342"/>
+        <polygon fill="#f7c400"/>
+        <polygon fill="#4fc3f7"/>
+        <polygon fill="#7cb342"/>
+        <polygon fill="#f7c400"/>
+        <polygon fill="#4fc3f7"/>
+        <polygon fill="#7cb342"/>
+        <polygon fill="#f7c400"/>
+        <polygon fill="#4fc3f7"/>
+        <polygon fill="#7cb342"/>
+        <polygon fill="#f7c400"/>
+        <polygon fill="#4fc3f7"/>
+        <polygon fill="#7cb342"/>
+    </g>
 
-    </svg>
-    </div>
+    <!-- Stars -->
+    <g fill="#f7c400">
+      <circle cx="350" cy="55" r="4"/>
+      <circle cx="520" cy="45" r="3"/>
+      <circle cx="690" cy="55" r="4"/>
+      <circle cx="860" cy="45" r="3"/>
+      <circle cx="1030" cy="55" r="4"/>
+      <circle cx="1200" cy="45" r="3"/>
+      <circle cx="1370" cy="55" r="4"/>
+    </g>
+
+  </svg>
+</div>
+<!-- RAMADAN DECOR -->
+<div class="ramadan-svg-decor">
+
+  <svg class="lantern left" viewBox="0 0 160 260" xmlns="http://www.w3.org/2000/svg">
+    <line x1="80" y1="0" x2="80" y2="35" stroke="#9c6a1a" stroke-width="4"/>
+    <path d="M55 35 L105 35 L120 60 H40 Z" fill="#e0a83a" stroke="#b17819" stroke-width="3"/>
+    <path d="M35 60 Q80 35 125 60 V165 Q80 195 35 165 Z"
+          fill="#f4a835" stroke="#b17819" stroke-width="4"/>
+    <path d="M55 75 Q80 60 105 75 V150 Q80 165 55 150 Z"
+          fill="#fff1c1" opacity=".9"/>
+    <path d="M35 80 Q50 70 65 80 V145 Q50 155 35 145 Z"
+          fill="#ffd27d" opacity=".85"/>
+    <path d="M95 80 Q110 70 125 80 V145 Q110 155 95 145 Z"
+          fill="#ffd27d" opacity=".85"/>
+    <path d="M45 165 H115 L100 195 H60 Z"
+          fill="#e0a83a" stroke="#b17819" stroke-width="3"/>
+  </svg>
+
+  <svg class="moon" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <radialGradient id="moonGlow">
+        <stop offset="0%" stop-color="#fff7d6"/>
+        <stop offset="100%" stop-color="#f4a835"/>
+        </radialGradient>
+
+        <mask id="crescentMask">
+        <rect width="120" height="120" fill="black"/>
+        <circle cx="60" cy="60" r="40" fill="white"/>
+        <circle cx="72" cy="52" r="40" fill="black"/>
+        </mask>
+    </defs>
+
+    <circle
+        cx="60"
+        cy="60"
+        r="40"
+        fill="url(#moonGlow)"
+        mask="url(#crescentMask)"
+    />
+  </svg>
+
+  <svg class="lantern right" viewBox="0 0 160 260" xmlns="http://www.w3.org/2000/svg">
+    <line x1="80" y1="0" x2="80" y2="35" stroke="#9c6a1a" stroke-width="4"/>
+    <path d="M55 35 L105 35 L120 60 H40 Z" fill="#e0a83a" stroke="#b17819" stroke-width="3"/>
+    <path d="M35 60 Q80 35 125 60 V165 Q80 195 35 165 Z"
+          fill="#f4a835" stroke="#b17819" stroke-width="4"/>
+    <path d="M55 75 Q80 60 105 75 V150 Q80 165 55 150 Z"
+          fill="#fff1c1" opacity=".9"/>
+    <path d="M35 80 Q50 70 65 80 V145 Q50 155 35 145 Z"
+          fill="#ffd27d" opacity=".85"/>
+    <path d="M95 80 Q110 70 125 80 V145 Q110 155 95 145 Z"
+          fill="#ffd27d" opacity=".85"/>
+    <path d="M45 165 H115 L100 195 H60 Z"
+          fill="#e0a83a" stroke="#b17819" stroke-width="3"/>
+  </svg>
+
 </div>
