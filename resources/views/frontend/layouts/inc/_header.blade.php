c8c6459245171dab8e564ff939429726a3fa1dc4
git <ahmedsalah511998@gmail.com>
2026-04-01 16:13:27 +0300

    save_changes_edits_change_customer_services_button_updates

--- resources/views/frontend/layouts/inc/_header.blade.php
+++ resources/views/frontend/layouts/inc/_header.blade.php
@@ -534,8 +534,8 @@ body.sticky-header-active {
             </nav>
 
             <div class="moreActions d-none d-lg-block">
-                <a href="{{route('all-workers')}}" class="cta-button">
-                    طلب استقدام
+                <a href="{{route('frontend.show.ourStaff')}}" class="cta-button">
+                    خدمة العملاء
                     <svg class="ms-2" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <line x1="7" y1="17" x2="17" y2="7"></line>
                         <polyline points="7 7 17 7 17 17"></polyline>
