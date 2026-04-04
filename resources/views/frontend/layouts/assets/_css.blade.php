b7302402719995b1463a350e90126653fbd62c9e
git <ahmedsalah511998@gmail.com>
2026-04-04 20:11:54 +0300

    save_changes_edits_updates

--- resources/views/frontend/layouts/assets/_css.blade.php
+++ resources/views/frontend/layouts/assets/_css.blade.php
@@ -4,33 +4,21 @@
 <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.rtl.min.css')}}" />
 <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
 
-<link rel="preload" href="{{asset('frontend/css/fontawesome.min.css')}}" as="style" onload="this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/swiper-bundle.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/animate.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/aos.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/dropify.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/jquery.fancybox.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 <link rel="preload" href="{{asset('frontend/css/odometer.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/select2.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/backEndFiles/validation/toastr.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/introjs.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="{{asset('frontend/css/introjs-rtl.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+
 
 
 <noscript>
   <link rel="stylesheet" href="{{asset('frontend/css/swiper-bundle.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/animate.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/aos.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/dropify.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/jquery.fancybox.min.css')}}">
   <link rel="stylesheet" href="{{asset('frontend/css/odometer.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/select2.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/backEndFiles/validation/toastr.min.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/backEndFiles/sweetalert/sweetalert.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/introjs.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/introjs-rtl.css')}}">
-  <link rel="stylesheet" href="{{asset('frontend/css/fontawesome.min.css')}}">
+
 </noscript>
 
 <link rel="preload" href="{{asset('frontend/cute-alert-master/style.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
@@ -38,12 +26,3 @@
   <link rel="stylesheet" href="{{asset('frontend/cute-alert-master/style.css')}}">
 </noscript>
 
-<link rel="preload"
-      as="style"
-      href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;700;800&family=Tajawal:wght@400;500;700&display=swap"
-      onload="this.onload=null;this.rel='stylesheet'">
-
-<noscript>
-<link rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;700;800&family=Tajawal:wght@400;500;700&display=swap">
-</noscript>
