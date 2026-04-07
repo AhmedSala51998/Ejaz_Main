8bcedcb5815df4dbb0f8e81126ff49a9da28a68c
git <ahmedsalah511998@gmail.com>
2026-04-07 13:48:34 +0300

    save_changes_edits_worker_page_updates

--- resources/views/frontend/layouts/assets/_js.blade.php
+++ resources/views/frontend/layouts/assets/_js.blade.php
@@ -13,6 +13,7 @@
 <script src="{{asset('frontend/js/dropify.min.js')}}" defer></script>
 <script src="{{asset('frontend/js/Custom.js')}}"></script>
 <script src="{{asset('frontend/js/app.js')}}" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script src="{{asset('frontend/backEndFiles/validation/jquery.form-validator.js')}}"></script>
 <script src="{{asset('frontend/backEndFiles/validation/toastr.min.js')}}"></script>
